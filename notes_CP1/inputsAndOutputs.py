e5306bbee38bc337565f53c59fa1713d07bf57da
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-08-20 10:22:11 -0600

    Inputs and Outputsnotes were completed

---
+++ notes_CP1/inputsAndOutputs.py
@@ -0,0 +1,15 @@
+#ML 2nd inputsand outputs notes
+
+#inputs give information into a program
+
+#make a input
+#input("what is your favorite color?")
+
+#inputs dont do much... UNLESS you add a variable
+#make a variable and a input
+color = ""
+color = input("what is your favorite color? ")
+
+#now do something with this input, use an output.
+
+print("is your favorite color "+color+"?")
