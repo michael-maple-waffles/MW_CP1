acf50ad23eb3b7ae4a97282da7cef07f367ef859
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-08-25 10:42:33 -0600

    Testing and polishing hello world

--- Practice/hello_world.py
+++ Practice/hello_world.py
@@ -9,7 +9,7 @@ while True:
     name_input = input("What is your name? ").capitalize().strip()
 
     if name_input in name:
-        print("hello",name_input".")
+        print("hello "+name_input+".")
     else:
         print("hello newbie.")
         name.append(name_input)
