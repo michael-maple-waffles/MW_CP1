9cafc01029b8a8a8deeeb0d0244806861e04e128
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-08-25 10:39:15 -0600

    finishing hello world

--- Practice/hello_world.py
+++ Practice/hello_world.py
@@ -1,5 +1,15 @@
 #ML 2nd Hello world project
 
 #classify variables
-name = ""
+name = [""]
+name_input = ""
 #inputs
+
+while True:
+    name_input = input("What is your name? ").capitalize().strip()
+
+    if name_input in name:
+        print("hello",name_input".")
+    else:
+        print("hello newbie.")
+        name.append(name_input)
