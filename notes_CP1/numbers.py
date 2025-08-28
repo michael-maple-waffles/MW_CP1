60bd62530d6b6f8322e590f20a9b0db1fa51e7f2
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-08-28 10:28:46 -0600

    finish numbers note

--- notes_CP1/numbers.py
+++ notes_CP1/numbers.py
@@ -5,10 +5,11 @@ weight = 0.00
 #integer
 count = 0
 
-#weight = input(float("insert how much your block weighs: "))
-#count = input(int("insert how many blocks there are: "))
+weight = float(input("insert how much your block weighs: "))
+count = int(input("insert how many blocks there are: "))
 
 #math
-toatle = weight + count
+total = weight * count
 print(8/4 * 2000 % 8 +5)
-#print("The toatle weight of all your blocks combined is "+toatle+".")
+print("The total weight of all your blocks combined is",total,"pounds.")
+
