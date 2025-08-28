f2f5aa1f2e20ada09ab3372b431eb242b92ca2b8
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-08-28 10:44:11 -0600

    couldnt figure out for loops, finished grade calculator.

---
+++ Practice/grade_calculator.py
@@ -0,0 +1,15 @@
+#MW_CP1 grade calculator practice
+
+print(" input the grades you have for all 7 of your classes")
+
+grade1 = float(input("What is your grade for your first class? "))
+grade2 = float(input("What is your grade for your second class? "))
+grade3 = float(input("What is your grade for your third class? "))
+grade4 = float(input("What is your grade for your forth class? "))
+grade5 = float(input("What is your grade for your fifth class? "))
+grade6 = float(input("What is your grade for your sixth class? "))
+grade7 = float(input("What is your grade for your seventh class? "))
+
+avg_grade = (grade1 + grade2 + grade3 + grade4 + grade5 + grade6 + grade7)/7
+
+print("Your average grade is:", round(avg_grade, 2), "have a good day.")
