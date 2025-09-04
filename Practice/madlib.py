14c944f22bb07e18c10348781d05180ab40abe9c
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-09-04 02:33:18 +0000

    finished mad lib

--- Practice/madlib.py
+++ Practice/madlib.py
@@ -15,6 +15,6 @@ user_inputs["actions"] = input("Give me a verb ending in ing! ")
 user_inputs["day_describer"] = input("What is the day like? ")
 
 
-story = "\n" + user_inputs["name"] + " was making a " + user_inputs["cookable"] + ". " + user_inputs["pronoun_regular"] + " has always enjoyed a good " + user_inputs["cookable"] + " on a " + user_inputs["day_describer"] + " day. Luckily " + user_inputs["name"] + " has always felt that times like this were perfect for turtle " + user_inputs["actions"] + ". Although many people found " + user_inputs["pronoun_possessive"] + " morning activities extroardinarily peculiar " + user_inputs["name"] + " still enjoyed every second of it. For they had lived " + user_inputs["age"] + " years, enough to give the maturity to understand how important doing the tasks you love is."
+story = "\n" + user_inputs["name"] + " was making a " + user_inputs["cookable"] + ". " + user_inputs["pronoun_regular"] + " has always enjoyed a good " + user_inputs["cookable"] + " on a " + user_inputs["day_describer"] + " day. Luckily " + user_inputs["name"] + " has always felt that times like this were perfect for turtle " + user_inputs["actions"] + ". Although many people found their morning activities extroardinarily peculiar " + user_inputs["name"] + " still enjoyed every second of it. For they had lived " + user_inputs["age"] + " years, enough to give the maturity to understand how important doing the tasks you love is."
 
 print(story)
