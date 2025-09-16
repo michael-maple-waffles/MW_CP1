320b3de72f9248c5e04b9ea9dfff674cdc46a02a
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-09-16 10:37:49 -0600

    started cave adventure project

---
+++ Self_projects/cave_adventure.py
@@ -0,0 +1,31 @@
+#MW_CP1 cave adventure
+
+
+#import libraries
+import random
+
+
+#character variable
+stell = {
+    #basic things
+    "hp" : 0,
+    "swipe" : random.randint(3 ,5),
+
+    #gameplay
+    "location" : 0,
+    "locate_X" : 1,
+    "locate_Y" : 1,
+    #abilities
+    "boulderwing_vest" : 0,
+    "starforce_dash" : 0,
+    "hyper_jump" : 0,
+    "bubble_trouble" : 0,
+    "starshot_power" : 0,
+}
+
+boulderwing = {
+    "hp" : 35,
+    "defeated" : False
+
+}
+
