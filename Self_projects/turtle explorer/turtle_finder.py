3fd6586899a5dbdbfd850203b2aab065c6da03b6
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-09-19 10:35:51 -0600

    turtle finder

---
+++ Self_projects/turtle explorer/turtle_finder.py
@@ -0,0 +1,10 @@
+import random
+
+turtle_find = random.randint(1,100)
+
+if turtle_find == 100:
+    print("you found two turtles")
+elif turtle_find >=75:
+    print("you found a turtle")
+else:
+    print("you didn't find a turtle")
