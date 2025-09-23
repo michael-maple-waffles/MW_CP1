95d69ffc454e31c6cdd2f8741fc57655356dab22
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-09-23 19:43:36 +0000

    setting up enemy classes

--- Self_projects/Cave adventure/cave_adventure_libraries.py
+++ Self_projects/Cave adventure/cave_adventure_libraries.py
@@ -3,7 +3,7 @@
 
 #import libraries
 import random
-
+import classes_in_game
 
 #character variable
 stell = {
@@ -24,31 +24,5 @@ stell = {
     
 }
 
-rollytolli = {
-    "hp" : 5,
-    "defeated" : False,
-    "zoom" : 3,
-    "zoom_bounce" : 5,
-}
-
-wonderwing = {
-    "hp" : 8,
-    "defeated" : False,
-    "dazzle" : random.randint(2,5),
-}
-
-boulderwing = {
-    "hp" : 35,
-    "position" : "neither",
-    "location_x" : 0,
-    "location_y" : 0,
-    "defeated" : False,
-    "boulder_vest" : random.randint(1,3),
-    "slam" : 8,
-    "slam_miss" : 4,
-    "falling_rubble" : 2,
-    "swoop_claw" : 5,
-    "swoop_miss" : random.randint(2,3),
-}
 
 
--- Self_projects/Cave adventure/classes_in_game.py
+++ Self_projects/Cave adventure/classes_in_game.py
@@ -1,3 +1,23 @@
 #MW_CP1 classes in game
 
+#enemy class
+class Enemy:
 
+    def __init__(self, enemy_type, hp, defeated,):
+        self.enemy_type = enemy_type
+        self.hp = hp
+        self.defeated = defeated
+    
+    def battle(self):
+        if self.enemy_type == "rollytolli":
+            pass
+        elif self.enemy_type == "wonderwing":
+            pass
+        elif self.enemy_type == "boulderwing":
+            pass
+
+rollytolli = Enemy("rollytolli", 5, False)
+
+wonderwing = Enemy("wonderwing", 8, False)
+
+boulderwing = Enemy("boulderwing", 35, False)
