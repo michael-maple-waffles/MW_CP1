dfa8b1b40a072e2c33aecebc0b64bce4779e091d
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-10-10 10:51:39 -0600

    password strength check started.

---
+++ Projects/password_strength.py
@@ -0,0 +1,16 @@
+#MW_CP1 password strength check.
+
+
+#variables: 
+    #password
+
+    #a dictionary that holds all past passwords PASSWORD_LIBRARY
+
+    #a class variable with six values five being a boolean that is correlated to the five requirements, the sixth being its score. PASSWORD_QUALITIES 
+
+#function to set up password to see if it meets requirments:
+    #check len of password if its greater than or equal to 8, 
+    # than PASSWORD_LIBRARY[PASSWORD].PASSWORD_QUALITIES(LENGTH) = True PASSWORD_LIBRARY[PASSWORD].PASSWORD_QUALITIES(PIONTS) += 1
+    #for  LETTER in PASSWORD:
+        #check if PASSWORD(KEY).capitalize is true
+
