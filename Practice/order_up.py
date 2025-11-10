9fb69e1673b9d3ef96c2182cdecdea722c195725
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-11-10 10:50:31 -0700

    wow

---
+++ Practice/order_up.py
@@ -0,0 +1,36 @@
+#MW_CP1 order up!
+
+#variables:
+#MENUE = Dictionary:
+
+    #MAINS: dictionary:
+
+    
+    #SIDES: dictionary:
+
+
+    #DRINKS: dictionary:
+
+#ORDER = Dictionary:
+
+    #MAINS: list:
+
+
+    #SIDES: list:
+
+
+    #DRINKS: list
+
+#Tiffany = Dictionary:
+
+    #line1
+
+    #line2
+
+    #line3
+
+    #line4
+
+    #linewrong
+
+    #order done
