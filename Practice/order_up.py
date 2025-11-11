7465193ac750c8d53ecf43ec8bbff6ab9e513deb
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-11-11 10:50:20 -0700

    you got this

--- Practice/order_up.py
+++ Practice/order_up.py
@@ -1,6 +1,9 @@
 #MW_CP1 order up!
 
 #variables:
+
+#checking = []
+
 #MENUE = Dictionary:
 
     #MAINS: dictionary:
@@ -10,6 +13,8 @@
 
 
     #DRINKS: dictionary:
+
+    #Desserts : dictionary:
 
 #ORDER = Dictionary:
 
@@ -21,16 +26,91 @@
 
     #DRINKS: list
 
-#Tiffany = Dictionary:
+    #DESSERTS : dictionary:
 
-    #line1
+#EMPLOYEES = Dictionary
 
-    #line2
+    #TIFFANY = Dictionary:
 
-    #line3
+        #line1
 
-    #line4
+        #line2
 
-    #linewrong
+        #line3
 
-    #order done
+        #line4
+
+        #linewrong
+
+        #order done
+
+    #CAROL = Dictionary
+
+        #line1
+
+        #line2
+
+        #line3
+
+        #line4
+
+        #linewrong
+
+        #order done
+
+    #DOUG = Dictionary
+
+        #line1
+
+        #line2
+
+        #line3
+
+        #line4
+
+        #linewrong
+
+        #order done
+
+    #STONER JOE = dictoinary
+
+        #line1
+
+        #line2
+
+        #line3
+
+        #line4
+
+        #linewrong
+
+        #order done
+
+
+#function that displays the menue (selection):
+
+    #choices.append(selection)
+
+    #for option in selection:
+
+        #choices.append(option)
+
+    #return choices
+
+
+
+
+#function that allows the user to select what they would like (SELECTION)
+
+    #while True:
+
+        #show emloyee's line for asking what dishes they would like
+
+        #if item not in MENUE[SELECTION].keys():
+
+            #employee reply item wrong
+
+        #else: #ORDER[SELECTION].append(item)
+
+
+
