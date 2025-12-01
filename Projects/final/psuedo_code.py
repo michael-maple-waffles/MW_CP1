c54db977fbd6b6e53df289d6e70dd0fb2c80544d
michael-maple-waffles <michael.willes@ucas-edu.net>
2025-12-01 10:50:33 -0700

    psuedo code

---
+++ Projects/final/psuedo_code.py
@@ -0,0 +1,89 @@
+#MW_CP1 Final Project Psuedo Code  ## hashs mean a maybe
+
+#variables:
+
+#Rin = {DICTIONARY of important things
+
+    #witchly lights collected : 0
+
+    #beastly lights collected : 1
+
+    ##beasts tamed : 0
+
+    #hope : 30
+
+    ##hope collected : 30-60
+
+    #lives : 1
+
+    ###lives collected 1-3
+
+    #city light : 0
+
+    #city dim : 10
+
+    #equip lantern : x
+
+    #equip light : y
+
+    #combo
+
+    #lanterns : {DICTIONARY of lanterns
+
+        #lantern : {DICTIONARY of the abilities of this lantern
+
+        #}
+
+        #weighted_lantern : {DICTIONARY of the abilities of this lantern
+
+        #}
+
+        #chainless lantern : {DICTIONARY of the abilities of this lantern
+
+        #}
+
+        #street lantern : {DICTIONARY of the abilities of this lantern
+
+        #}
+    #}
+
+    #lights = {DICTIONARY of all the lights and there effects
+
+        #flame : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #lightning : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #steam : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #snow : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #wind : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #stars : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #moon : {DICTIONARY of all the effects of this light
+
+        #}
+
+        #sun : {DICTIONARY of all the effects of this light
+
+        #}
+    #}
+
+    #MOVEMENT : {DICTIONARY of all different movement effects
+
+    #}
+#}
+
